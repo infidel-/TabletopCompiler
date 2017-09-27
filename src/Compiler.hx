9c33041434a0fd5c46bd2d8d862dc9da4ca02062
Max Kowarski <starinfidel@gmail.com>
2017-09-27 22:30:43 +0400

    TTS export

--- src/Compiler.hx
+++ src/Compiler.hx
@@ -13,10 +13,15 @@ class Compiler
 {
   var game: _GameConfig;
   var options: _Options;
+  var tts: _TTSSave;
+  var currentDeckID: Int;
 
   public function new()
     {
       game = null;
+      tts = null;
+      options = null;
+      currentDeckID = 99;
     }
 
 
@@ -50,6 +55,16 @@ class Compiler
       // create temp dirs
       Sys.command('mkdir', [ '-p', 'tmp', 'results' ]);
 
+      // parse TTS save
+//      if (game.tabletopSimulator != null)
+        {
+          p('Loading Tabletop Simulator template ' +
+            game.tabletopSimulator.template + '...');
+
+          var json = File.getContent(game.tabletopSimulator.template);
+          tts = Json.parse(json);
+        }
+
       var t0 = Sys.time();
       for (t in game.tasks)
         {
@@ -73,6 +88,12 @@ class Compiler
           var time = Std.int(Sys.time() - t1);
           p('Task took ' + time + ' seconds.');
         }
+
+      // save TTS file
+      p('Saving TTS file ' + game.tabletopSimulator.result + '...');
+      var json = Json.stringify(tts, null, "  ");
+      File.saveContent(
+        'results/' + game.tabletopSimulator.result, json);
 
       var time = Std.int(Sys.time() - t0);
       p('Build took ' + time + ' seconds.');
@@ -117,6 +138,7 @@ class Compiler
     {
       // cleanup
       runCommand('rm', [ '-f', 'tmp/card*.png' ]);
+      currentDeckID++;
 
       p('Reading stats ' + task.stats + '...');
       var statsFile = File.getContent(task.stats);
@@ -133,12 +155,61 @@ class Compiler
         if (colnames[i] == '%AMOUNT')
           colAmount = i;
 
+      // find first card in placeholder deck
+      // we will use it as an object template
+      var ttsObj: _TTSObject = null;
+      var deckName = '$' + task.id;
+      for (o in tts.ObjectStates)
+        if (o.Nickname == deckName)
+          {
+            ttsObj = o;
+            break;
+          }
+      if (ttsObj == null)
+        {
+          p('No placeholder TTS deck for ' + deckName);
+          Sys.exit(1);
+        }
+
+      // init some common fields
+      ttsObj.Name = 'DeckCustom';
+      ttsObj.Nickname = task.name;
+      ttsObj.DeckIDs = [];
+      ttsObj.CustomDeck = {};
+      var tplCard = ttsObj.ContainedObjects[0];
+      ttsObj.ContainedObjects = [];
+      var ttsDeck: _TTSCustomDeck = {
+        FaceURL: game.tabletopSimulator.prefix +
+          'results/' + task.result,
+        BackURL: game.tabletopSimulator.prefix +
+          'results/' + task.back,
+        NumWidth: 0,
+        NumHeight: 0,
+        BackIsHidden: false,
+        UniqueBack: false,
+      };
+      Reflect.setField(ttsObj.CustomDeck, '' + currentDeckID,
+        ttsDeck);
+
+/*
+      for (f in Reflect.fields(ttsObj))
+        trace(f + ': ' + Reflect.field(ttsObj, f));
+*/
+
       // generate single card files
       var cardID = 1;
       var numCards = 0;
       for (row in stats)
         {
+          // init TTS card
+          var ttsCardID = currentDeckID * 100 + cardID;
+          ttsObj.DeckIDs.push(ttsCardID);
+          var ttsCard: _TTSCard = Reflect.copy(tplCard);
+          ttsCard.CardID = ttsCardID;
+          ttsObj.ContainedObjects.push(ttsCard);
+
           var str = tpl;
+          var cardTitle = null;
           for (i in 0...row.length)
             {
               // out of bounds
@@ -156,10 +227,15 @@ class Compiler
               if (key.indexOf('.') > 0)
                 key = key.substr(0, key.indexOf('.'));
               var val = row[i];
+              if (key == "$TITLE")
+                cardTitle = val;
 
 //              trace(key + ': ' + val);
               str = StringTools.replace(str, key, val);
             }
+
+          if (cardTitle != null)
+            ttsCard.Nickname = cardTitle;
 
           // generate HTML file
           // needs to be in the same dir for relative links to work
@@ -174,32 +250,50 @@ class Compiler
           ]);
           numCards++;
 
-          // if card has amounts
-          var amount = Std.parseInt(row[colAmount]);
-          if (amount > 1)
+          // in non-optimized mode make copies of cards
+          if (game.optimizeDecks)
             {
-              p('Making ' + amount + ' copies...');
+              var amount = Std.parseInt(row[colAmount]);
               while (amount-- > 1) // -1 for original
                 {
-                  numCards++;
-                  Sys.command('cp', [
-                    '-f',
-                    'tmp/card' + cardID + '.png',
-                    'tmp/card' + cardID +
-                      '_copy' + amount + '.png',
-                  ]);
+                  ttsObj.ContainedObjects.push(ttsCard);
+                  ttsObj.DeckIDs.push(ttsCardID);
+                }
+            }
+          else
+            {
+              // if the card has amounts
+              // make multiple copies of the same card for
+              // simple deck composition
+              var amount = Std.parseInt(row[colAmount]);
+              if (amount > 1)
+                {
+                  p('Making ' + amount + ' copies...');
+                  while (amount-- > 1) // -1 for original
+                    {
+                      numCards++;
+                      Sys.command('cp', [
+                        '-f',
+                        'tmp/card' + cardID + '.png',
+                        'tmp/card' + cardID +
+                          '_copy' + amount + '.png',
+                      ]);
+                    }
                 }
             }
 
           cardID++;
         }
       p('Generated ' + numCards + ' cards');
-
-      // compose deck image
-      p('Composing ' + task.result + '...');
       var deckWidth = task.deckWidth;
       if (deckWidth > numCards)
         deckWidth = numCards;
+      ttsDeck.NumWidth = deckWidth;
+      ttsDeck.NumHeight = Std.int(numCards / deckWidth);
+//      trace(ttsObj);
+
+      // compose deck image
+      p('Composing ' + task.result + '...');
       runCommand('montage', [
         'tmp/card*.png',
         '-geometry', '100%',
@@ -248,6 +342,12 @@ class Compiler
 typedef _GameConfig = {
   var name: String;
   var version: String;
+  var optimizeDecks: Bool;
+  var tabletopSimulator: {
+    var template: String;
+    var prefix: String;
+    var result: String;
+  };
   var tasks: Array<_TaskConfig>;
 }
 
@@ -257,6 +357,7 @@ typedef _TaskConfig = {
   var id: String;
   var name: String;
   var result: String;
+  var back: String;
   var stats: String;
   var lang: Array<String>;
   var template: String;
@@ -272,3 +373,38 @@ typedef _Options = {
   var tasks: Array<String>;
 }
 
+
+// partial TTS save file mapping
+
+typedef _TTSSave = {
+  var SaveName: String;
+  var ObjectStates: Array<_TTSObject>;
+}
+
+typedef _TTSObject = {
+  var Name: String;
+  var Nickname: String;
+  var Description: String;
+  var DeckIDs: Array<Int>;
+  var CustomDeck: Dynamic;
+  var ContainedObjects: Array<_TTSCard>;
+  var GUID: String;
+}
+
+typedef _TTSCard = {
+  var Name: String;
+  var Nickname: String;
+  var Description: String;
+  var CardID: Int;
+  var GUID: String;
+}
+
+
+typedef _TTSCustomDeck = {
+  var FaceURL: String;
+  var BackURL: String;
+  var NumWidth: Int;
+  var NumHeight: Int;
+  var BackIsHidden: Bool;
+  var UniqueBack: Bool;
+}
